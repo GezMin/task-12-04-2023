e4ee90e8c785cb1b48e91b182ac95403632b7223
GezMin <s.shvetsov.v@gmail.com>
2023-04-17 12:48:30 +0600

    fix bugs style

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const resultItog = document.querySelector('.result-itog')
 const resultItogObj = document.querySelector('.result-itog-obj')
 
 const calcBtn = document.querySelector('.calc__btn button')
+const error = document.querySelector('.error')
 
 const changeSum = () => {
     let arr = [
@@ -27,24 +28,31 @@ const changeSum = () => {
         parseFloat(material.value),
         parseFloat(smallPrice.value),
     ]
-    let sum = ((arr[1] + arr[2]) * 70) / 100 + (arr[3] * 30) / 100
-    saveWithoutKit.innerHTML = Math.round(sum)
-    let sumlost = arr[1] + arr[2] + arr[3] - sum
-    lostWithoutKit.innerHTML = Math.round(sumlost)
-    saveWithoutKitObj.innerHTML = Math.round(sum * arr[0])
-    lostWithoutKitObj.innerHTML = Math.round(sumlost * arr[0])
 
-    let sumKit = arr[1] + arr[2] + (arr[3] * 90) / 100
-    saveWithKit.innerHTML = Math.round(sumKit)
-    let sumlostKit = arr[1] + arr[2] + arr[3] - sumKit
-    lostWithKit.innerHTML = Math.round(sumlostKit)
-    saveWithKitObj.innerHTML = Math.round(sumKit * arr[0])
-    lostWithKitObj.innerHTML = Math.round(sumlostKit * arr[0])
+    if (arr.every(val => val && val !== 0 && val !== '.')) {
+        error.innerHTML = ''
+        let sum = ((arr[1] + arr[2]) * 70) / 100 + (arr[3] * 30) / 100
+        saveWithoutKit.innerHTML = Math.round(sum) + ' ₽'
+        let sumlost = arr[1] + arr[2] + arr[3] - sum
+        lostWithoutKit.innerHTML = Math.round(sumlost) + ' ₽'
+        saveWithoutKitObj.innerHTML = Math.round(sum * arr[0]) + ' ₽'
+        lostWithoutKitObj.innerHTML = Math.round(sumlost * arr[0]) + ' ₽'
 
-    resultItog.innerHTML = Math.round(sumlost - sumlostKit) + ' ₽'
-    resultItogObj.innerHTML = Math.round(sumlost - sumlostKit) * arr[0] + ' ₽'
+        let sumKit = arr[1] + arr[2] + (arr[3] * 90) / 100
+        saveWithKit.innerHTML = Math.round(sumKit) + ' ₽'
+        let sumlostKit = arr[1] + arr[2] + arr[3] - sumKit
+        lostWithKit.innerHTML = Math.round(sumlostKit) + ' ₽'
+        saveWithKitObj.innerHTML = Math.round(sumKit * arr[0]) + ' ₽'
+        lostWithKitObj.innerHTML = Math.round(sumlostKit * arr[0]) + ' ₽'
 
-    return arr
+        resultItog.innerHTML = Math.round(sumlost - sumlostKit) + ' ₽'
+        resultItogObj.innerHTML = Math.round(sumlost - sumlostKit) * arr[0] + ' ₽'
+
+        return arr
+    } else {
+        error.innerHTML = 'Заполните все поля!'
+        return
+    }
 }
 
 changeSum()
